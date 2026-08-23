7e4fb141be9b5e813207fd612e29eb28dcf03608
matteo-cadei <matteocadei98@gmail.com>
2026-08-23 14:50:17 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = "a-tavola-v1";
+
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./recipe.html",
+  "./new.html",
+  "./edit.html",
+  "./calendar.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
